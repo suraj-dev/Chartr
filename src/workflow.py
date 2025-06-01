80e56f6fcf266ccb1a1d4687627123976ae40da8
Suraj Nagaraj <suraj.scorp11@gmail.com>
2025-05-31 23:36:28 -0400

    Observability - added Otel and jaeger
    * added otel and jaeger for observability
    
    * remove comment
    
    ---------
    
    Co-authored-by: Suraj Nagaraj <surajnagaraj@Surajs-MacBook-Pro.local>

--- src/workflow.py
+++ src/workflow.py
@@ -3,39 +3,44 @@ from src.query import extract_db_schema, nl_to_sql
 from src.db import get_connection
 from src.types import WorkflowState
 from src.visualize import get_chart_type, plot_data
+from src.observability import tracer
 
 def convert_nl_to_sql(state: WorkflowState):
-    nl_query = state["nl_query"]
-    sql_query = nl_to_sql(nl_query)
-    print("Converted NL to SQL:", sql_query)
-    return {"sql_query": sql_query}
+    with tracer.start_as_current_span("convert_nl_to_sql"):
+        nl_query = state["nl_query"]
+        sql_query = nl_to_sql(nl_query)
+        print("Converted NL to SQL:", sql_query)
+        return {"sql_query": sql_query}
 
 def execute_sql(state: WorkflowState):
     sql_query = state["sql_query"]
     try:
-        conn = get_connection()
-        cursor = conn.cursor()
-        cursor.execute(sql_query)
-        results = cursor.fetchall()
-        column_names = [desc[0] for desc in cursor.description]
-        return {"results": results, "column_names": column_names}
+        with tracer.start_as_current_span("execute_sql"):
+            conn = get_connection()
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            results = cursor.fetchall()
+            column_names = [desc[0] for desc in cursor.description]
+            return {"results": results, "column_names": column_names}
     except Exception as e:
         print(f"Error executing SQL query: {e}")
         return {"results": []}
 
 def visualize_results(state: WorkflowState):
-    results = state["results"]
-    print(results)
-    chart_config = state["chart_config"]
-    columns = state["column_names"]
-    plot_data(results, chart_config.get("chart_type"), columns, chart_config.get("title"))
-    return {}
+    with tracer.start_as_current_span("visualize_results"):
+        results = state["results"]
+        print(results)
+        chart_config = state["chart_config"]
+        columns = state["column_names"]
+        plot_data(results, chart_config.get("chart_type"), columns, chart_config.get("title"))
+        return {}
 
 def get_chart_recommendation(state: WorkflowState):
-    nl_query = state["nl_query"]
-    sql_query = state["sql_query"]
-    chart_config = get_chart_type(nl_query, sql_query, extract_db_schema())
-    return {"chart_config": chart_config}
+    with tracer.start_as_current_span("get_chart_recommendation"):
+        nl_query = state["nl_query"]
+        sql_query = state["sql_query"]
+        chart_config = get_chart_type(nl_query, sql_query, extract_db_schema())
+        return {"chart_config": chart_config}
 
 def create_workflow():
     graph = StateGraph(WorkflowState)
@@ -53,7 +58,8 @@ def create_workflow():
     return graph.compile()
 
 def run_workflow(nl_query):
-    graph = create_workflow()
-    initial_state = {"nl_query": nl_query}
-    result = graph.invoke(initial_state)
-    return result
+    with tracer.start_as_current_span("run_workflow"):
+        graph = create_workflow()
+        initial_state = {"nl_query": nl_query}
+        result = graph.invoke(initial_state)
+        return result
