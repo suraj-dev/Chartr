f88089831eca0102029738e56f6ebaa184f29205
Suraj Nagaraj <surajnagaraj@Surajs-MacBook-Pro.local>
2025-05-31 23:33:49 -0400

    added otel and jaeger for observability

---
+++ src/observability.py
@@ -0,0 +1,21 @@
+# src/observability.py (create this)
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# Set up the tracer provider and Jaeger exporter
+resource = Resource.create({"service.name": "chartr"})
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name=os.getenv("JAEGER_HOST"),
+    agent_port=int(os.getenv("JAEGER_PORT")),
+)
+span_processor = BatchSpanProcessor(jaeger_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
